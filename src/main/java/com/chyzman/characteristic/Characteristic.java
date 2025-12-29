c4047c86ffde530714033f6158973ad44d16f4d4
Basique <basiqueevangelist@yandex.ru>
2025-12-30 01:16:27 +0300

    add packet to make client know that the profile has changed; fix stuff

--- src/main/java/com/chyzman/characteristic/Characteristic.java
+++ src/main/java/com/chyzman/characteristic/Characteristic.java
@@ -1,7 +1,10 @@
 package com.chyzman.characteristic;
 
 import com.chyzman.characteristic.command.CharacterCommand;
+import com.chyzman.characteristic.network.SwitchGameProfileS2CPayload;
+import io.wispforest.owo.serialization.CodecUtils;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.resources.ResourceLocation;
 
 public class Characteristic implements ModInitializer {
@@ -10,6 +13,8 @@ public class Characteristic implements ModInitializer {
     @Override
     public void onInitialize() {
         CharacterCommand.register();
+
+        PayloadTypeRegistry.configurationS2C().register(SwitchGameProfileS2CPayload.TYPE, CodecUtils.toPacketCodec(SwitchGameProfileS2CPayload.ENDEC));
     }
 
     public static ResourceLocation id(String path) {
---
+++ src/main/java/com/chyzman/characteristic/mixin/client/ClientConfigurationPacketListenerImplAccessor.java
@@ -0,0 +1,14 @@
+package com.chyzman.characteristic.mixin.client;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ClientConfigurationPacketListenerImpl.class)
+public interface ClientConfigurationPacketListenerImplAccessor {
+    @Accessor
+    @Mutable
+    void setLocalGameProfile(GameProfile gameProfile);
+}
